ebce0ad773b442e4727aa94f7da9db26c2a92d91
Augusto Zanellato <augusto.zanellato@gmail.com>
2025-03-31 11:50:37 +0200

    placeholder: http: fix domain regex, enable check

--- placeholders/http/subdomain.js
+++ placeholders/http/subdomain.js
@@ -1,6 +1,6 @@
 // Check subdomain matches
 const subdomain = window.location.host.split(".")[0];
-// const re = new RegExp("^[a-z0-9-]+-[a-f0-9]{16}$");
+const re = new RegExp("^[a-z0-9-]+-[a-f0-9]{8,16}$");
 const parser = new DOMParser();
 
 function checkChallengeReady() {
@@ -14,7 +14,7 @@ function checkChallengeReady() {
 						// Check if 200 res has same title as this loading page
 						const doc = parser.parseFromString(text, "text/html")
 						if (doc.title !== document.title) {
-							location.reload(true)
+							location.reload()
 						}
 					})
 			} else { console.log("instance not ready") }
@@ -24,10 +24,9 @@ function checkChallengeReady() {
 
 document.addEventListener("DOMContentLoaded", function () {
 	// Check if subdomain matches
-	// if (re.test(subdomain)) {
-	if (subdomain) {
+	if (re.test(subdomain)) {
 		document.getElementsByClassName("challenge-loading")[0].style.display = "block";
-		const POLL_INTERVAL = 10000;
+		const POLL_INTERVAL = 2000;
 		setInterval(() => checkChallengeReady(), POLL_INTERVAL);
 	} else {
 		document.getElementsByClassName("wrong-domain")[0].style.display = "block";
